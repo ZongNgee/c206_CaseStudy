3ad746a8acd26d0678bc478f78dae6398a1473d3
Jeniffer1976 <21025290@myrp.edu.sg>
2022-08-08 23:10:09 +0800

    Updated Parent and Student class

---
+++ C206_CaseStudy/src/Parent.java
@@ -0,0 +1,39 @@
+public class Parent extends Student{
+
+	private String parentName;
+	private String email;
+	private int contactNo;
+	
+	public Parent(String studentID, String name, String s_Class, String teacher, 
+			String parentName, String email, int contactNo) {
+		super(studentID, name, s_Class, teacher);
+		this.parentName = parentName;
+		this.email = email;
+		this.contactNo = contactNo;
+	}
+
+	public String getParentName() {
+		return parentName; 
+	}
+
+	public void setParentName(String parentName) {
+		this.parentName = parentName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public int getContactNo() {
+		return contactNo;
+	}
+
+	public void setContactNo(int contactNo) {
+		this.contactNo = contactNo;
+	}
+	
+}
---
+++ C206_CaseStudy/src/Student.java
@@ -0,0 +1,78 @@
+public class Student{
+
+	private String studentID;
+	private String name;
+	private String s_Class;
+	private String teacher;
+	private int registrationID;
+	private boolean isRegistered;
+	
+	public Student (String studentID, String name, String s_Class, String teacher) {
+		this.studentID = studentID;
+		this.name = name;
+		this.s_Class = s_Class;
+		this.teacher = teacher;
+		this.registrationID = 0;
+		this.isRegistered = false;
+	}
+
+	public String getStudentID() {
+		return studentID;
+	}
+
+	public void setStudentID(String studentID) {
+		this.studentID = studentID;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getS_Class() {
+		return s_Class;
+	}
+
+	public void setS_Class(String s_Class) {
+		this.s_Class = s_Class;
+	}
+
+	public String getTeacher() {
+		return teacher;
+	}
+
+	public void setTeacher(String teacher) {
+		this.teacher = teacher;
+	}
+	
+	
+	public int getRegistrationID() {
+		return registrationID;
+	}
+
+	public void setRegistrationID(int registrationID) {
+		this.registrationID = registrationID;
+	}
+
+	public boolean isRegistered() {
+		return isRegistered;
+	}
+
+	public void setRegistered(boolean isRegistered) {
+		this.isRegistered = isRegistered;
+	}
+
+	public String getStatus() {
+		if (isRegistered) {
+			return "Successful";
+		} else {
+			return "Waiting list";
+		}
+	}
+
+	
+	
+}

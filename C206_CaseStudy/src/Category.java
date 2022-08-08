f4f5cc8b006d86cb7ba7d8cb699d2ae0b02bbccc
Jeniffer1976 <21025290@myrp.edu.sg>
2022-08-08 17:19:57 +0800

    category and cca classes

---
+++ C206_CaseStudy/src/Category.java
@@ -0,0 +1,19 @@
+
+public class Category {
+	private String catName;
+
+	public Category(String catName) {
+		this.catName = catName;
+	}
+
+	public String getCatName() {
+		return catName;
+	}
+
+	public void setCatName(String catName) {
+		this.catName = catName;
+	} 
+	
+	
+
+}

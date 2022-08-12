65377c88c236a500d8e54cb8fba5fc5e70c7915d
2473nicole <21011122@myrp.edu.sg>
2022-08-12 14:14:30 +0800

    maintain student

--- C206_CaseStudy/src/C206_CaseStudy.java
+++ C206_CaseStudy/src/C206_CaseStudy.java
@@ -13,20 +13,62 @@ public class C206_CaseStudy {
 		ArrayList<CCA> ccaList = new ArrayList<CCA>();
 		ccaList.add(new CCA("Uniform Group", "Red Cross", "Youths learning basic first aid", 20, "Friday", "3PM", "Hall", "Mr Teng"));
 		ccaList.add(new CCA("Perfoming Arts", "Dance", "Youths uniting as one to dance", 30, "Friday", "3.30PM", "Dance Studio", "Mrs Woo Woo"));
-	
+		
+		ArrayList<Parent> parentListTest = new ArrayList<Parent>();
+		parentListTest.add(new Parent("123456", "Peter", "C206", "Armstong", "David", "123456@myrp.edu.sg", 99999999));
+		
+		C206_CaseStudy.viewAllParent(parentListTest);
+		
+//		C206_CaseStudy.deleteAllParent(parentListTest);
 		
 		C206_CaseStudy.viewAllCategories(categoriesList);
 		
 		C206_CaseStudy.viewAllCCAs(ccaList);
 		//===========================================================
 		
+		//Maintaining student accounts (add,view,delete)
+		ArrayList<Student> studentList = new ArrayList<>();
+		int option = 0;
+
+	    while (option != 4) {
+
+	      C206_CaseStudy.Studentmenu();
+	      option = Helper.readInt("Enter an option > ");
+
+	      if (option == 1) {
+	        // Add a new item
+	        C206_CaseStudy.setHeader("ADD");
+	          // Add student
+	          Student student = inputStudent();
+	          C206_CaseStudy.addStudent(studentList, student);
+	          System.out.println("Parent added");
+	       
+
+	      } else if (option == 2) {
+	        // View all items
+	        C206_CaseStudy.viewAllStudent(studentList);
+
+	      } else if (option == 3) {
+	        // Delete Student
+	        C206_CaseStudy.setHeader("DELETE");      
+	          // Delete parent
+	        C206_CaseStudy.deleteAllStudent(studentList);
+
+	      } else if (option == 4) {
+	        System.out.println("Bye!");
+	        
+	      } else {
+	        System.out.println("Invalid option");
+	      }
+	    }
+		//===================================================================
 		
-		
+			
 		
 		
 		//Maintaining parent accounts (add,view,delete)
 		ArrayList<Parent> parentList = new ArrayList<Parent>();
-		int option = 0;
+//		int option = 0;
 
 	    while (option != 4) {
 
@@ -111,7 +153,79 @@ public class C206_CaseStudy {
 		System.out.println(output);
 		
 	}
+	// For maintaining Student accounts ==========================================================
 	
+			 public static void Studentmenu() {
+			      C206_CaseStudy.setHeader("Maintaining student accounts");
+			      System.out.println("1. Add Student");
+			      System.out.println("2. View Student");
+			      System.out.println("3. Delete Student");
+			      System.out.println("4. Quit");
+			      Helper.line(80, "-");
+			    }
+			 
+			 //add student account
+			 public static Student inputStudent() {
+				 
+			     String studentID = Helper.readString("Enter your student ID > ");
+			     String student_Name = Helper.readString("Enter student Name > ");
+			     String student_Class =  Helper.readString("Enter student class > ");
+			     String teacher = Helper.readString("Enter classroom teacher > ");
+
+
+			     Student student = new Student(studentID, student_Name, student_Class, teacher);
+			     return student;
+			      
+			    }
+			 public static void addStudent(ArrayList<Student> studentList, Student student) {
+			      
+			      studentList.add(student);
+			    }
+			 //view student account
+			 public static String retrieveAllStudent(ArrayList<Student> studentList) {
+				 String output = "";
+				 for (int i = 0; i < studentList.size(); i++) {
+
+				        output += String.format("%-15s %-15s %-30s %-15s %-15s\n",
+				        	i+1,
+				        	studentList.get(i).getStudentID(),
+				        	studentList.get(i).getName(), 
+				        	studentList.get(i).getS_Class(), 
+				        	studentList.get(i).getTeacher());
+				      }
+				      return output;
+				    }
+			 public static void viewAllStudent(ArrayList<Student> studentList) {
+			      C206_CaseStudy.setHeader("STUDENT LIST");
+			      String output = String.format("%-15s %-15s %-30s %-15s %-15s\n","POSITION",
+			    		  "Student ID","Student Name", "Student Class","Class Teacher");
+			      output += retrieveAllStudent(studentList);
+			      System.out.println(output);
+			    }
+			 
+			 public static void deleteAllStudent(ArrayList<Student> studentList) {
+				 String output = String.format("%-15s %-15s %-30s %-15s %-15s\n", "POSITION",
+						 "Student ID", "Student Name","Student Class","Class Teacher");
+				 for (int i = 0; i < studentList.size(); i++) {
+				 output += String.format("%-15s %-15s %-30s %-15s %-15s\n",
+				        	i+1,
+				        	studentList.get(i).getStudentID(),
+				        	studentList.get(i).getName(), 
+				        	studentList.get(i).getS_Class(), 
+				        	studentList.get(i).getTeacher());
+				 
+				 int delOption = Helper.readInt("Choose option to delete > ");
+				 if (delOption == (i+1)) {
+					 studentList.remove(i);
+			    	 System.out.println("Student deleted");
+				 }else {
+					 System.out.println("Invalid option");
+				 }
+				 System.out.println(output);
+			     }
+			 }
+			//===========================================================================================		
+		    
 	
 	// For maintaining parent accounts ==========================================================
 	
@@ -123,6 +237,7 @@ public class C206_CaseStudy {
 	      System.out.println("4. Quit");
 	      Helper.line(80, "-");
 	    }
+	 
 	 //add parent account
 	 public static Parent inputParent() {
 		 
@@ -134,8 +249,8 @@ public class C206_CaseStudy {
 	     String email = Helper.readString("Enter parent email > ");
 	     int contact = Helper.readInt("Enter your contact number > ");
 
-	     Parent Parent = new Parent(studentID, student_Name, student_Class, teacher, name, email, contact);
-	     return Parent;
+	     Parent parent = new Parent(studentID, student_Name, student_Class, teacher, name, email, contact);
+	     return parent;
 	      
 	    }
 	 public static void addParent(ArrayList<Parent> parentList, Parent parent) {
